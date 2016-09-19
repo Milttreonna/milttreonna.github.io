0a33cdb6d9b379c07f13845cb7b05d53c152c6fa
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-09-19 15:15:55 -0500

    updating pictionary

--- gladiator/gladiator.js
+++ gladiator/gladiator.js
@@ -1,21 +1,7 @@
 function something() {alert("WE DID SOMETHING!");}
 
 
-var other= function (health) {health=100}
-var self = function (health) {health=100}
-var showHealth=document.getElementById("health");
+var health = document.getElementById("health")
+var attack = document.getElementById("attack")
 
-var lowdamage= Math.floor((Math.random() * 20) + 11);
-var highdamage= Math.floor((Math.random() * 30) + 21);
-
-# when one player presses attack button, the others health should go down
-var attack = function (self, other) {
-  myAttack= document.getElementById('attack');
-
-  var hit=(Math.random()* self.lowdamage, + self.highdamage));
-  if (hit > 20) {
-    hit *= 2 ; alert("RAGE!");
-  }
-  else {hit};
-  other.health =+ hit
-}
+function attack() { var attack = health.value - (Math.floor((Math.random() * 30) + 10));}
