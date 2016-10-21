4f3d62d03501fabc78712b5afe83954923e468ee
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-10-21 15:18:22 -0500

    moving stuff to new repository

--- gladiator/gladiator.js
+++
@@ -1,7 +0,0 @@
-function something() {alert("WE DID SOMETHING!");}
-
-
-var health = document.getElementById("health")
-var attack = document.getElementById("attack")
-
-function attack() { var attack = health.value - (Math.floor((Math.random() * 30) + 10));}
